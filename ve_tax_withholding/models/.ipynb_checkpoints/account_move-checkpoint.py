4f664278bd911d897d0f3b3f6e6c721472489430
avpayall <avpayall@gmail.com>
2022-01-10 19:03:46 +0000

    [ADD] smart button logic function

--- ve_tax_withholding/models/.ipynb_checkpoints/account_move-checkpoint.py
+++ ve_tax_withholding/models/.ipynb_checkpoints/account_move-checkpoint.py
@@ -2,4 +2,21 @@
 from odoo import models, fields, api
 class AccountMove( models.Model):
     _inherit = 'account.move'
-    control_number = fields.Char(string='Numero de control', required=True, default='00')                                               
+    control_number = fields.Char(string='Numero de control', required=True, default='00')
+    
+    @api.model
+    def (self):
+    	view_id = self.env.ref('modul_name._id_of_form_view').id
+    	context = self._context.copy()
+    	return {
+            'name':'form_name',
+            'view_type':'form',
+            'view_mode':'tree',
+            'views' : [(view_id,'form')],
+            'res_model':'model_name',
+            'view_id':view_id,
+            'type':'ir.actions.act_window',
+            'res_id':self.id,
+            'target':'new',
+            'context':context,
+        }
