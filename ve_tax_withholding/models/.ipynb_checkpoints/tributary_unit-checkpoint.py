d450bc4476878c00c32c0b7bd7cea3750b9124e9
willberOdoo <mailto:willrr.payall.2021@gmail.com>
2022-01-13 18:07:11 +0000

    [FIX] tributary unit field view

--- ve_tax_withholding/models/.ipynb_checkpoints/tributary_unit-checkpoint.py
+++ ve_tax_withholding/models/.ipynb_checkpoints/tributary_unit-checkpoint.py
@@ -6,7 +6,7 @@ class TributaryUnit(models.Model):
     _name = 'tax.tributary_unit'
     _description = 'model for tributary  unit'
     
-    unit = fields.Float(string='Unidad Tributaria', required = True, store= True)
-    factor = fields.Float(string='Factor Fiscal', required = True, store= True)
-    minimum = fields.Float(string='Minimo', store= True)
+    #unit = fields.Float(string='Unidad Tributaria', required = True, store= True)
+    #factor = fields.Float(string='Factor Fiscal', required = True, store= True)
+    #minimum = fields.Float(string='Minimo', store= True)
     
